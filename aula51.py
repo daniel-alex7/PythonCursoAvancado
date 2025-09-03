4b16c650b815b2d1a77172a7b3f238e8c5bb62fd
danim-se <130123798+danim-se@users.noreply.github.com>
2025-09-03 11:31:08 -0300

    Atualizando aula 51

---
+++ aula51.py
@@ -0,0 +1,19 @@
+"""
+Exercício 
+
+Exiba os índices da lista
+0 Maria
+1 Helena 
+2 Luiz
+
+"""
+
+lista = ['Maria', 'Helena', 'Luiz']
+
+# lista está em str e indices são int, devemos fazer o seguinte para velos:
+
+
+for indice in range(len(lista)):
+    
+    print('Os indices são: ',indice)
+    indice += 1
