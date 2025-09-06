a25327c2f4f55122ce804328347d9000d6253221
danim-se <130123798+danim-se@users.noreply.github.com>
2025-09-06 16:02:44 -0300

    Aula 60

---
+++ aula60.py
@@ -0,0 +1,17 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condição> else <outro valor>
+"""
+# condi = 10 == 10
+# var = 'VALOR' if condi else 'Outro valor'
+# print(var)
+
+# para cpf
+
+# digito = 9
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito >= 9 else digito
+# print(novo_digito)
+
+
+print('VALOR' if True else 'Outro valor' if True else 'Fim')
