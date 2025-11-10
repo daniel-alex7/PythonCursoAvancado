b1c706cf5ab41a8f35ce48d69147bf43e1973ca1
danim-se <130123798+danim-se@users.noreply.github.com>
2025-11-10 13:17:06 -0300

    Atualizando ex

--- Aulas/aula105.py
+++ Aulas/aula105.py
@@ -1,17 +1,29 @@
 #  Decoradores com parâmetros
-
-def decorada(func):
-    print('Decoradora 1')
+def fabrica_de_decoradores(a=None, b=None, c=None): #pegar parametros do decorador
+   
+    def fabrica_de_funcoes(func): #decorador em si
+        print('Decoradora 1')
     
-    def aninhada(*args, **kwargs):
-        print('Aninhada')
-        res = func(*args, **kwargs)
-        return res
-    return aninhada
+        def aninhada(*args, **kwargs): #o que vamos executar
+            print('Parametro do decorador, ',a, b, c)
+            print('Aninhada')
+            res = func(*args, **kwargs)
+            return res
+        return aninhada
+    return fabrica_de_funcoes
 
-@decorada
+
+
+@fabrica_de_decoradores(1, 2, 3)
 def soma(x, y):
     return x + y
-    
-# dez_mais_cinco = soma(10, 5)
-# print(dez_mais_cinco)
+
+decoradora = fabrica_de_decoradores()
+multiplica = decoradora(lambda x,y: x * y)
+
+dez_mais_cinco = soma(10, 5)
+dez_vezes_cinco = multiplica(10, 5)
+
+
+print(dez_mais_cinco)
+print(dez_vezes_cinco)
---
+++ Aulas/aula108.py
@@ -0,0 +1,21 @@
+# Considerando duas lista de inteiros ou fgloats (listaA e listaB)
+# Some os valores nas listas retornando uma nova lista com os valores somados:
+
+#Se uma lista for maior que a outra, a soma só vai considerar o tamanho menor
+
+#Exemplo:
+
+#lista_a = [1, 2, 3, 4, 5, 6, 7]
+#lista_b = [1, 2. 3, 4]
+
+#resultado;
+#lista_soma = [2, 4, 6, 8]
+
+from itertools import zip_longest
+
+l1 = [1, 2, 3, 4, 5, 6, 7]
+l2 = [1, 2, 3, 4]
+
+soma = l1 + l2
+
+print(list(zip(soma)))
