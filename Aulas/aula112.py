b2a8c37f4b51371f678f9d15064248dd5e6fa9ed
danim-se <130123798+danim-se@users.noreply.github.com>
2025-12-27 11:58:50 -0300

    Filter e Reduce

---
+++ Aulas/aula112.py
@@ -0,0 +1,23 @@
+# filter é um filtro funcional 
+
+def print_iter(iterator): 
+    print(*list(iterator), sep='\n')
+    print()
+    
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 12.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+def filtrar_preco(produto):
+    return produto['preco'] > 100
+
+novos_produtos = filter(
+    filtrar_preco,
+    produtos
+)
+
+print_iter(produtos)
