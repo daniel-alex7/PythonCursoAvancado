5918caf951c9b4eeffc6c3389856d78d02480601
danim-se <130123798+danim-se@users.noreply.github.com>
2026-01-08 16:15:15 -0300

    Aprendendo sobre JSON e resolvendo problema de mutaveis em def

---
+++ Aulas/aula118.py
@@ -0,0 +1,20 @@
+# Problemas de parâmetros mutáveis em funções Python
+
+def adiciona_clientes(nome, lista=None): #Isso não é bom em fazer 
+    if lista is None:
+        lista = []
+    lista.append(nome)
+    return lista
+
+
+# lista1 = []
+
+cliente1 = adiciona_clientes('Daniel')
+adiciona_clientes('Carlos', cliente1)
+adiciona_clientes('Fernando', cliente1)
+print(cliente1)
+
+cliente2 = adiciona_clientes('Davi')
+adiciona_clientes('Carlota', cliente2)
+
+print(cliente2)
