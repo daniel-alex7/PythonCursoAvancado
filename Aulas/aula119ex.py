06b92bdb99285df5b75d83c1a76fd0159189f500
danim-se <130123798+danim-se@users.noreply.github.com>
2026-01-13 12:16:48 -0300

    Atualizando exercicios

---
+++ Aulas/aula119ex.py
@@ -0,0 +1,104 @@
+
+def listar(tarefas):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa para listar')
+        return 
+    print()
+          
+    print('Tarefas')
+    for tarefa in tarefas:
+        print(f'\t{tarefa}')
+    print()
+        
+def desfazer(tarefas, tarefas_refazer):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa para listar')
+        return
+          
+    tarefa = tarefas.pop()
+    print(f'{tarefa=} foi removida')
+    tarefas_refazer.append(tarefa)
+    print()
+    
+    
+def refazer(tarefas, tarefas_refazer):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa para refazer')
+        return
+          
+    tarefa = tarefas_refazer.pop()
+    print(f'{tarefa=} foi adicionada na lista de tarefas')
+    tarefas.append(tarefa)
+    print()
+        
+def adicionar(tarefa, tarefas):
+    print()
+    tarefa = tarefa.strip()
+    if not tarefa:
+        print('Você não digitou tarefa')
+        return
+    
+    print(f'{tarefa=} foi adicionada na lista de tarefas')
+    tarefas.append(tarefa)
+    print()
+        
+                
+tarefas = []
+tarefas_refazer = []
+   
+while True:
+    print('Comandos: listar, desfazer e refazer')
+    tarefa = input('Digite uma tarefa ou comando: ')
+    
+    if tarefa == 'listar':
+        listar(tarefas)
+        continue
+    
+    elif tarefa == 'desfazer' :
+        desfazer(tarefas, tarefas_refazer)
+        listar(tarefas)
+        continue
+    
+    elif tarefa == 'refazer':
+        refazer(tarefas, tarefas_refazer)
+        listar(tarefas)
+        continue
+    
+    else:
+        adicionar(tarefa, tarefas)
+        listar(tarefas)
+        continue
+        
+  
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+   
+
+ 
+
+
+
+
+
+         
+
+
+
+
+
+
