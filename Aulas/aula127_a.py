886884214398592fe6e5a727914f4019ddfa966d
danim-se <130123798+danim-se@users.noreply.github.com>
2026-01-21 17:08:37 -0300

    Correção

--- Aulas/aula127_a.py
+++ Aulas/aula127_a.py
@@ -7,18 +7,44 @@
 
 import json
 
+# class Pessoa:
+#         def __init__(self, nome, idade):
+#             self.nome = nome
+#             self.idade = idade
+
+
+# p1 = Pessoa('Daniel', 21)
+
+    
+# with open('aula127_a.json', 'w', encoding='utf8') as arquivo:
+ 
+#     json.dump(
+#             p1.__dict__, arquivo, indent=4,
+#     )
+
+# Correção: 
+
+CAMINHO_ARQUIVO = 'aula127.json'
+
 class Pessoa:
         def __init__(self, nome, idade):
             self.nome = nome
             self.idade = idade
 
+            
+p1 = Pessoa('Daniel', 20)
+p2 = Pessoa('Juliana', 18)
+p3 = Pessoa('Elio', 20)
 
-p1 = Pessoa('Daniel', 21)
+bd = [vars(p1), vars(p2), vars(p3)]
 
-    
-with open('aula127_a.json', 'w', encoding='utf8') as arquivo:
- 
-    json.dump(
-            p1.__dict__, arquivo, indent=4,
-    )
 
+def fazer_dump():
+        with open(CAMINHO_ARQUIVO, 'w') as arquivo:
+                print('FAZENDO DUMP')
+                json.dump(bd, arquivo, ensure_ascii = False, indent = 2)
+        
+        
+if __name__ == '__main__':
+        print('ELE É O MAIN')
+        fazer_dump()
--- Aulas/aula127_b.py
+++ Aulas/aula127_b.py
@@ -9,31 +9,53 @@
 
 import json
 
-class Pessoa:
-        def __init__(self, nome, idade):
-            self.nome = nome
-            self.idade = idade
+# class Pessoa:
+#         def __init__(self, nome, idade):
+#             self.nome = nome
+#             self.idade = idade
 
 
 
    
     
 
-with open('aula127_a.json', 'r', encoding='utf8') as arquivo:
-    p1 = json.load(arquivo)
-    print(p1)
+# with open('aula127_a.json', 'r', encoding='utf8') as arquivo:
+#     p1 = json.load(arquivo)
+#     print(p1)
 
     
-with open('aula127_b.json', 'w', encoding='utf8') as arquivo:
-    p1 = Pessoa('Roberto', 21)
+# with open('aula127_b.json', 'w', encoding='utf8') as arquivo:
+#     p1 = Pessoa('Roberto', 21)
 
-    json.dump(
-            p1.__dict__, arquivo, indent=4,
-    )
+#     json.dump(
+#             p1.__dict__, arquivo, indent=4,
+#     )
     
     
-print(p1.nome, p1.idade)
+# print(p1.nome, p1.idade)
+
+# Correção
+
+from aula127_a import CAMINHO_ARQUIVO, Pessoa, fazer_dump
+
+fazer_dump()
+
+
+
+with open(CAMINHO_ARQUIVO, 'r') as arquivo:
+    pessoas = json.load(arquivo)
     
+    
+    p1 = Pessoa(**pessoas[0])
+    p2 = Pessoa(**pessoas[1])
+    p3 = Pessoa(**pessoas[2])
+    
+    
+    print(p1.nome, p1.idade)
+    print(p2.nome, p2.idade)
+    print(p3.nome, p3.idade)
+        
+print(__name__)
     
     
     
