7dbca024d9580c27462c17cb2e325a525054e495
danim-se <130123798+danim-se@users.noreply.github.com>
2026-01-23 14:41:01 -0300

    Um pouco sobre connection

--- Aulas/aula130.py
+++ Aulas/aula130.py
@@ -2,3 +2,41 @@
 # method - self, método de instância
 # @classmethod - cls, método de classe
 # @staticmethod - método estático (❌self, ❌cls)
+
+class Connection:
+    def __init__(self, host='localhost'):
+        self.host = host
+        self.user = None
+        self.password = None
+        
+    def set_user(self, user):
+        #setter
+        self.user = user
+        
+    def set_password(self, password):
+        self.password = password
+        
+    @classmethod
+    def create_with_auth(cls, user, password):
+        connection = cls()
+        connection.user = user
+        connection.password = password
+        return connection
+    
+    @staticmethod
+    def log(msg):
+        print('LOG', msg)
+            
+
+c1 = Connection.create_with_auth('Daniel', '1234')
+ 
+        
+# c1 = Connection()
+
+# c1.set_user(input('Digite seu nome: '))
+# c1.set_password(input('Digite sua senha:'))
+
+print(Connection.log('Essa é a mensagem de log'))
+
+print(c1.user)
+print(c1.password)
