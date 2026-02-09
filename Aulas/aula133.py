7b0197a8e882d4f8471145da1476570036c8d814
danim-se <130123798+danim-se@users.noreply.github.com>
2026-02-09 16:14:50 -0300

    encapsulamento em python (convenção de devs)

---
+++ Aulas/aula133.py
@@ -0,0 +1,54 @@
+# Encapsulamento (modificadores de acesso: public, protected, private)
+# Python NÃO TEM modificadores de acesso
+# Mas podemos seguir as seguintes convenções
+#   (sem underline) = public
+#       pode ser usado em qualquer lugar
+# _ (um underline) = protected
+#       não DEVE ser usado fora da classe
+#       ou suas subclasses.
+# __ (dois underlines) = private
+#       "name mangling" (desfiguração de nomes) em Python
+#       _NomeClasse__nome_attr_ou_method
+#       só DEVE ser usado na classe em que foi
+#       declarado.
+
+from functools import partial
+
+
+class Foo: 
+    def __init__(self):
+        self.public = 'isso é publico'
+        self._protected = 'isso é protegido'
+        self.__private = 'isso é private'
+        
+        
+        
+    def metodo_publico(self):
+        print(self.__private)
+        self.__metodo_private()
+        return 'metodo publico'
+    
+    #uso interno da classe
+    def _metodo_protected(self):
+        print('_metodo_protected')
+        return '_metodo_protected'
+    
+    def __metodo_private(self):
+        print('_metodo_private')
+        return '_metodo_private'
+        
+    
+    
+    
+    
+        
+f = Foo()
+
+
+# print(f._protected)
+
+# print(f.public)
+# print(f.metodo_publico())
+
+# Só assim para executar com name mangling, mas essa pratica não é recomendada
+print(f._Foo__metodo_private())
