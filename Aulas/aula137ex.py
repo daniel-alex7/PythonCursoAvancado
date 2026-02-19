e64a6616752295fbfe9f6738996be87022bf30c3
danim-se <130123798+danim-se@users.noreply.github.com>
2026-02-19 16:49:28 -0300

    Correção

--- Aulas/aula137ex.py
+++ Aulas/aula137ex.py
@@ -8,42 +8,97 @@
 # Obs.: Um fabricante pode fabricar varios carros
 # Exiba o nome do carro, motor e  fabricantes na tela
 
+# class Carro:
+#     def __init__(self, nome):
+#         self.nome = nome
+#         self._motor = None
+#         self.fabricante = None
+        
+#     def inserir_motor(self, motor):
+#         self.motor = motor
+
+#     def inserir_fabricante(self, fabricante):
+#         self.fabricante = fabricante
+    
+# #Associação    
+# class Motor:
+#     def __init__(self, nome):
+#         self.nome = nome
+               
+
+# #Associação
+# class Fabricante:
+#     def __init__(self, nome):
+#         self.nome = nome
+#         self.carros = []
+        
+#     def inserir_carro(self, carro) :
+#         self.carros.append(carro)
+        
+    
+# carro1 = Carro('Carro: HB20')
+# motor1 = Motor('Motor: V8')
+# fabrincante1 = Fabricante('Fabricante: Chetrolet')
+
+# carro1.inserir_motor(motor1)
+# carro1.inserir_fabricante(fabrincante1)
+# fabrincante1.inserir_carro(carro1)
+
+# print(f'Carro: {carro1.nome}')
+# print(f'Motor: {motor1.nome}')
+# print(f'Fabricante: {fabrincante1.nome}')
+
+
+# correção: 
+
 class Carro:
     def __init__(self, nome):
         self.nome = nome
         self._motor = None
-        self.fabricante = None
+        self._fabricante = None
         
-    def inserir_motor(self, motor):
-        self.motor = motor
-
-    def inserir_fabricante(self, fabricante):
-        self.fabricante = fabricante
+    @property
+    def motor(self):
+        return self._motor
     
-#Associação    
-class Motor:
-    def __init__(self, nome):
-        self.nome = nome
-               
-
-#Associação
-class Fabricante:
-    def __init__(self, nome):
-        self.nome = nome
-        self.carros = []
-        
-    def inserir_carro(self, carro) :
-        self.carros.append(carro)
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
         
     
-carro1 = Carro('Carro: HB20')
-motor1 = Motor('Motor: V8')
-fabrincante1 = Fabricante('Fabricante: Chetrolet')
+    @property
+    def fabricante(self):
+        return self._fabricante
+    
+    @fabricante.setter
+    def fabricante(self, valor):
+        self._fabricante = valor
+                
+        
+class Motor: 
+    def __init__(self, nome):
+        self.nome = nome
+        
 
-carro1.inserir_motor(motor1)
-carro1.inserir_fabricante(fabrincante1)
-fabrincante1.inserir_carro(carro1)
+class Fabricante: 
+    def __init__(self, nome):
+        self.nome = nome
+        
+        
+fusca = Carro('Fusca')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+fusca.fabricante = volkswagen
+fusca.motor = motor_1_0
 
-print(f'Carro: {carro1.nome}')
-print(f'Motor: {motor1.nome}')
-print(f'Fabricante: {fabrincante1.nome}')
+print(fusca.nome, fusca.fabricante.nome, fusca.motor.nome)
+print()
+
+hb20 = Carro('Hb20')
+motor_1_2 = Motor('1.2')
+chetrolet = Fabricante('Chetrolet')
+hb20.fabricante = chetrolet
+hb20.motor = motor_1_2
+
+print(hb20.nome, hb20.fabricante.nome, hb20.motor.nome)
+print()
