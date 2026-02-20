c002ad854fd2f8a5b583285d3ac6758c0208177a
danim-se <130123798+danim-se@users.noreply.github.com>
2026-02-20 16:43:39 -0300

    Um pouco mais sobre herança

--- Aulas/aula138.py
+++ Aulas/aula138.py
@@ -9,3 +9,34 @@
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
 
+
+
+
+class Pessoa: 
+    cpf = '1234'
+    
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        
+    def falar_nome_classe(self):
+        print('Classe Pessoa')
+        print(self.nome, self.sobrenome, self.__class__.__name__)#pega o nome da classe
+        
+class Cliente(Pessoa):
+        
+    def falar_nome_classe(self):
+        print('EITA NEM SAI DA CLASSE CLIENTE')
+        print(self.nome, self.sobrenome, self.__class__.__name__)
+    
+class Aluno(Pessoa):
+    cpf = 'cpf aluno'
+    
+c1 = Cliente("Daniel", "Robson")
+c1.falar_nome_classe()
+a1 = Aluno("Juliana", "Alexandre")
+a1.falar_nome_classe()
+print(a1.cpf)
+# help(Cliente)    
+    
+    
