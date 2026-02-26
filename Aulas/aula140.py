5cb3fcff3b9c8822cc93ee9e096b0e8b20c9a206
danim-se <130123798+danim-se@users.noreply.github.com>
2026-02-26 13:49:58 -0300

    Prática

--- Aulas/aula140.py
+++ Aulas/aula140.py
@@ -8,7 +8,7 @@
 # Animal -> Mamifero -> Humano -> Pessoa -> Cliente
 # Cliente(Pessoa, FileLog)
 #
-# A, B, C, D
+# C, B, C, D
 # D(B, C) - C(A) - B(A) - A
 #
 # método -> falar
@@ -33,3 +33,28 @@ class A:
     
     def quem_sou(self):
         print('A')
+        
+
+class B(A): 
+    ...
+    
+    # def quem_sou(self):
+    #     print('B')
+        
+class C(A): 
+    ...
+    
+    def quem_sou(self):
+        print('C')
+        
+class D(B, C): 
+    ...
+    
+    # def quem_sou(self):
+    #     print('D')
+        
+d = D()
+d.quem_sou()
+
+# print(D.__mro__)
+print(D.mro())
