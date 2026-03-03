63585aab0fb3668397b947f1314df252c7434ef1
danim-se <130123798+danim-se@users.noreply.github.com>
2026-03-03 16:44:35 -0300

    Metade da parte 2

--- Aulas/aula141/log.py
+++ Aulas/aula141/log.py
@@ -1,12 +1,20 @@
 # Abstração 
+# Herança - é um
 
 class Log:
     def log(self, msg):
         raise NotImplementedError('Implemente método log')
+    
+    def log_error(self, msg):
+        return self.log(f'Error: {msg}')
        
 class LogFileMixin(Log):
     def log(self, msg):
         print(msg)
+        
+class LogPrintMixin(Log):
+    def log(self, msg):
+        print(msg)        
        
 if __name__ == '__main__':
     l = LogFileMixin()
