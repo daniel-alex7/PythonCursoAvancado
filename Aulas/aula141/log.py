d6eafd8b85080f3da5e62c3f2bf1a15b30706506
danim-se <130123798+danim-se@users.noreply.github.com>
2026-03-04 12:57:26 -0300

    Aula 141 part 4, composição e herança multipla

--- Aulas/aula141/log.py
+++ Aulas/aula141/log.py
@@ -1,21 +1,39 @@
 # Abstração 
 # Herança - é um
 
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente método log')
     
     def log_error(self, msg):
-        return self.log(f'Error: {msg}')
+        return self._log(f'Error: {msg}')
+    
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
        
 class LogFileMixin(Log):
-    def log(self, msg):
-        print(msg)
+    def _log(self, msg):
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando arquivo no log: ', msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
         
 class LogPrintMixin(Log):
-    def log(self, msg):
-        print(msg)        
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')        
        
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('Alguma coisa')        
+    lp = LogPrintMixin()
+    lp.log_error('Alguma coisa')        
+    lp.log_success('Que legal')
+    
+    
+    lf = LogFileMixin()
+    lf.log_error('Alguma coisa')        
+    lf.log_success('Que legal')
+    print(LOG_FILE)
