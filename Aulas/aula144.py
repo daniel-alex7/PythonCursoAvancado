cf79a74e2e0282791b69ee8ca68aac88a6d167ef
danim-se <130123798+danim-se@users.noreply.github.com>
2026-03-06 16:41:07 -0300

    Aula de polimofirmos na prática

--- Aulas/aula144.py
+++ Aulas/aula144.py
@@ -11,3 +11,37 @@
 # Princípio da substituição de liskov
 # Objetos de uma superclasse devem ser substituíveis
 # por objetos de uma subclasse sem quebrar a aplicação.
+#  Sobrecarga de métodos (overload) = Não Py
+# Sobreposição de métodos (override) = Sim Py
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, mensagem):
+         self.mensagem = mensagem
+      
+    @abstractmethod   
+    def enviar(self) -> bool: ...
+        
+class NotificacaoEmail(Notificacao):  
+    def enviar(self):
+        print('Email: enviando - ', self.mensagem)
+        return True
+    
+class NotificacaoSMS(Notificacao):  
+    def enviar(self):
+        print('SMS: enviando - ', self.mensagem)
+        return True
+        
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+    
+    if notificacao_enviada:
+        print('Notificação enviada')
+    else:
+        print('Notificação não enviada')
+        
+notificacao_email = NotificacaoEmail('Testando e-mail')
+notificar(notificacao_email)
+print()
+notificacao_sms = NotificacaoSMS('Testando SMS')
+notificar(notificacao_sms)
