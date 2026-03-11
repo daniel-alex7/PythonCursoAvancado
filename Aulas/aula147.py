00ae0c2d6943fa1e8664e5481043d86abe0155aa
danim-se <130123798+danim-se@users.noreply.github.com>
2026-03-11 13:54:11 -0300

    representação de str

--- Aulas/aula147.py
+++ Aulas/aula147.py
@@ -15,3 +15,30 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+
+
+class Ponto():
+    def __init__(self, x, y, z = 'String'):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    # para representação em str
+    def __str__(self):
+        return f'({self.x}), ({self.y})'    
+        
+    # Estou passando para outro desenvolvedor comunicação de como obejto seja criado
+    def __repr__(self):
+        # class_name = self.__class__.__name__
+        class_name = type(self).__name__
+        return f'{class_name}: (x={self.x}), (y={self.y}), (z={self.z!r})'
+    
+        
+p1 = Ponto(1, 2)
+p2 = Ponto(982, 392)
+
+print(p1)
+
+#ver repr
+print(repr(p2))
+print(f'{p2!r}') #s no lugar do r -> seria str
