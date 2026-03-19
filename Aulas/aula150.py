ef809c814a4272f57542494bb2372d4e0b027c2a
danim-se <130123798+danim-se@users.noreply.github.com>
2026-03-19 11:22:59 -0300

    Aula 150

--- Aulas/aula150.py
+++ Aulas/aula150.py
@@ -1,2 +1,22 @@
 # Context Manager com função - Criando e Usando gerenciadores de contexto
 
+from contextlib import contextmanager
+
+@contextmanager
+def my_open(caminho_arquivo, modo):
+    try:
+        print('Abrindo arquivo')
+        arquivo = open(caminho_arquivo, modo, encoding='utf8')
+        yield arquivo
+        
+    except Exception as e:
+        print('Ocrreu erro', e)
+    finally:
+        print('Fechando o arquivo')
+        arquivo.close()
+
+with my_open('Aulas/aula150.txt', 'w') as arquivo:
+    arquivo.write('Linha A \n')
+    arquivo.write('Linha B \n')
+    arquivo.write('Linha C \n')
+    print('WITH', arquivo)
