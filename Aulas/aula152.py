b474c486777e6bad6653e276ccd81514089ec32a
danim-se <130123798+danim-se@users.noreply.github.com>
2026-03-24 10:30:35 -0300

    Aual 1252 inicio

---
+++ Aulas/aula152.py
@@ -0,0 +1,45 @@
+# Funções decoradoras e decoradores com classes
+
+
+def meu_repr(self):
+    class_name = self.__class__.__name__
+    class_dict = self.__dict__
+    class_repr = f'{class_name}({class_dict})'
+    return class_repr
+    
+def adiciona_repr(cls):
+    cls.__repr__ = meu_repr
+    return cls
+
+def meu_planeta(metodo):
+    def interno(self, *args, **kwargs):
+        resultado = self.falar_nome() 
+@adiciona_repr
+class Time:
+    def __init__(self, nome):
+        self.nome = nome
+        
+@adiciona_repr        
+class Planeta:
+    def __init__(self, nome):
+        self.nome = nome
+        
+    def falar_nome(self):
+        return f'O planeta é {self.nome}'
+ 
+   
+brasil = Time('Brasil')
+portugal = Time('Portugal')
+
+
+terra = Planeta('Terrra')
+marte = Planeta('Marte')
+
+print(brasil)
+print(portugal)
+
+print(marte)
+print(terra)
+
+print(terra.falar_nome())
+print(marte.falar_nome())
