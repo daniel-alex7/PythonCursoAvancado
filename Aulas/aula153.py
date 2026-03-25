6c90c0bbfeece27444c90d273e144fff84e0ea74
danim-se <130123798+danim-se@users.noreply.github.com>
2026-03-25 12:03:15 -0300

    Aula 153 sobre __call__

---
+++ Aulas/aula153.py
@@ -0,0 +1,17 @@
+# Método especial __call__
+# callable é algo que pode ser executado com parênteses 
+# Em classes normas, __call__ faz a instância de uma 
+# classe "callable"
+
+
+class CallMe:
+    def __init__(self, phone):
+        self.phone = phone
+        
+    def __call__(self, nome):
+        print(nome, 'está chamando ', self.phone)
+        return 123
+
+call1 = CallMe('23232342343')
+retorno = call1('Daniel R')
+print(retorno)
