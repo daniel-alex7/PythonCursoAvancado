0dca784b68264d334b57c0926edf5c784f84b3c9
danim-se <130123798+danim-se@users.noreply.github.com>
2026-04-01 11:11:24 -0300

    DocString

--- Aulas/aula155.py
+++ Aulas/aula155.py
@@ -34,8 +34,47 @@
 # print(type(f))
 # print(type(Foo))
 
+
+
+# AULA 1
+
+# def meu_repr(self):
+#     return f'{type(self).__name__}({self.__dict__})'
+
+# class Meta(type):
+#     def __new__(mcs, name, bases, dct):
+#         print('METACLASS NEW')
+#         cls = super().__new__(mcs, name, bases, dct)
+#         cls.attr = 1234
+#         cls.__repr__ = meu_repr
+        
+#         if 'falar' not in cls.__dict__:
+#             raise NotImplementedError('IMPLEMENTE FALAR')
+        
+#         return cls
+
+# class Pessoa(metaclass=Meta):
+#     def __new__(cls, *args, **kwargs):
+#         print('MEU NEW')
+#         instancia = super().__new__(cls)
+#         return instancia
+    
+#     def __init__(self, nome):
+#         print('MEU INIT')
+#         self.nome = nome
+        
+#     def falar(self):
+#         print('FALANDO...')
+        
+# p1 = Pessoa('Daniel')
+# print(p1.attr)
+# print(p1)
+        
+# AULA 2
+
 def meu_repr(self):
     return f'{type(self).__name__}({self.__dict__})'
+
 
 class Meta(type):
     def __new__(mcs, name, bases, dct):
@@ -43,26 +82,37 @@ class Meta(type):
         cls = super().__new__(mcs, name, bases, dct)
         cls.attr = 1234
         cls.__repr__ = meu_repr
-        
-        if 'falar' not in cls.__dict__:
-            raise NotImplementedError('IMPLEMENTE FALAR')
-        
+
+        if 'falar' not in cls.__dict__ or \
+                not callable(cls.__dict__['falar']):
+            raise NotImplementedError('Implemente falar')
+
         return cls
 
+    def __call__(cls, *args, **kwargs):
+        instancia = super().__call__(*args, **kwargs)
+
+        if 'nome' not in instancia.__dict__:
+            raise NotImplementedError('Crie o attr nome')
+
+        return instancia
+
+
 class Pessoa(metaclass=Meta):
+    # falar = 123
+
     def __new__(cls, *args, **kwargs):
         print('MEU NEW')
         instancia = super().__new__(cls)
         return instancia
-    
+
     def __init__(self, nome):
         print('MEU INIT')
-        self.nome = nome
-        
+        # self.nome = nome
+
     def falar(self):
         print('FALANDO...')
-        
-p1 = Pessoa('Daniel')
-print(p1.attr)
-print(p1)
-        
+
+
+p1 = Pessoa('Luiz')
+p1.falar()
---
+++ Aulas/aula156/main.py
@@ -0,0 +1,11 @@
+# import uma_linha
+# import varias_linha
+import documentado_funcao
+
+# print(dir(uma_linha))
+# print(uma_linha.__doc__)
+# print(uma_linha.__file__)
+# print(uma_linha.__name__)
+
+
+help(documentado_funcao)
