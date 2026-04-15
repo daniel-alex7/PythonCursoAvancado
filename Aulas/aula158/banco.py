02f345ce84aa0da3820ce35b3ee50ba753ad4b22
danim-se <130123798+danim-se@users.noreply.github.com>
2026-04-15 11:32:31 -0300

    Bancos

---
+++ Aulas/aula158/banco.py
@@ -0,0 +1,32 @@
+import contas
+import pessoas
+
+class Banco:
+    def __init__ (
+        self,
+        agencias: list[int] | None = None,
+        clientes: list[pessoas.Pessoa] | None = None,
+        contas: list[contas.Conta] | None = None,
+    ) -> None:
+        self.agencias = agencias or []
+        self.clientes = clientes or []
+        self.contas = contas or []
+        
+    def _checa_agencia(self, conta):
+        if conta.agencia in self.agencias:
+            return True
+        return False
+        
+    def _checa_cliente(self, cliente):
+        if cliente in self.clientes:
+            return True
+        return False
+        
+    def _checa_conta(self, conta):
+        if conta in self.contas:
+            return True
+        return False
+      
+        
+    def autenticar (self, cliente, conta):
+        ...
