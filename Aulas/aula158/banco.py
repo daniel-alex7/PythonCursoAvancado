9e54179944178fe8e4e861fc0d48760b246827e1
danim-se <130123798+danim-se@users.noreply.github.com>
2026-04-16 12:31:58 -0300

    FInalizando

--- Aulas/aula158/banco.py
+++ Aulas/aula158/banco.py
@@ -15,18 +15,55 @@ class Banco:
     def _checa_agencia(self, conta):
         if conta.agencia in self.agencias:
             return True
+        print('_checa _agencia', True)
         return False
         
     def _checa_cliente(self, cliente):
         if cliente in self.clientes:
             return True
+        print('_checa _cliente', True)
         return False
         
     def _checa_conta(self, conta):
         if conta in self.contas:
             return True
+        print('_checa _conta', True)
+        return False
+    
+    def _checa_se_conta_e_do_cliente(self, cliente, conta):
+        if conta in self.contas:
+            print('_checa_se_conta_e_do_cliente', True)
+            return True
+        print('_checa_se_conta_e_do_cliente', False)
         return False
       
         
-    def autenticar (self, cliente, conta):
-        ...
+    def autenticar (self, cliente: pessoas.Pessoa, conta: contas.Conta):
+        return self._checa_agencia(conta) and \
+            self._checa_cliente(cliente) and \
+            self._checa_conta(conta) and \
+            self._checa_se_conta_e_do_cliente(cliente, conta)
+    
+    
+    def __repr__(self) -> str:
+        class_name = type(self).__name__
+        attrs = f'({self.agencias!r}, {self.clientes!r}, {self.contas!r})'
+        return f'{class_name}{attrs}'            
+
+if __name__ == '__main__':
+    c1 = pessoas.Cliente('Daniel', 20)
+    cc1 = contas.ContaCorrente(111, 222, 0, 0)
+    c1.conta = cc1
+    c2 = pessoas.Cliente('Juliana', 20)
+    cp1 = contas.ContaPoupanca(777, 444, 1000)
+    c2.conta = cp1
+    banco = Banco()
+    banco.clientes.extend([c1, c2])
+    banco.contas.extend([cc1, cp1])
+    banco.agencias.extend([111, 222])
+    
+    
+    if banco.autenticar(c1, cc1):
+        cc1.depositar(10)
+        c1.conta.depositar(100)
+        print(c1.conta)
