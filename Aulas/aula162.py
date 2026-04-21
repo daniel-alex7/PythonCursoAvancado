aaea5be72acaa5732b8320ed451c047e62a4bbf0
danim-se <130123798+danim-se@users.noreply.github.com>
2026-04-21 11:54:28 -0300

    Organizando

--- Aulas/aula162.py
+++ Aulas/aula162.py
@@ -10,7 +10,7 @@
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 # Instalando o pytz
 # pip install pytz types-pytz 
-# from datetime import datetime  
+from datetime import datetime  
 
 # part 1
 
@@ -24,33 +24,8 @@
 
 #  part 2
 
-# data = datetime.now()
-# print(data.timestamp())  # Isso está na base de dados
-# print(datetime.fromtimestamp(1776726108.490952))
+data = datetime.now()
+print(data.timestamp())  # Isso está na base de dados
+print(datetime.fromtimestamp(1776726108.490952))
 
-# part 3
 
-# datetime.timedelta e dateutil.relativetimedelta (calculando datas)
-# Docs:
-# https://dateutil.readthedocs.io/en/stable/relativedelta.html
-# https://docs.python.org/3/library/datetime.html#timedelta-objects
-from datetime import datetime
-
-from dateutil.relativedelta import relativedelta
-
-fmt = '%d/%m/%Y %H:%M:%S'
-data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
-data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
-# delta = timedelta(days=10, hours=2)
-delta = relativedelta(data_fim, data_inicio)
-print(delta.days, delta.years)
-# print(data_fim - delta)
-# print(data_fim)
-# print(data_fim + relativedelta(seconds=60, minutes=10))
-
-# delta = data_fim - data_inicio
-# print(delta.days, delta.seconds, delta.microseconds)
-# print(delta.total_seconds())
-# print(data_fim > data_inicio)
-# print(data_fim < data_inicio)
-# print(data_fim == data_inicio)
